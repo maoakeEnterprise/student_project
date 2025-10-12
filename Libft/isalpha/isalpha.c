7bf331f45a92f76ccbe2a8b6ea4e79cd97de98d8
Maoake <maoake.terii@gmail.com>
2025-10-12 18:33:20 +0000

    finishing the first

--- Libft/isalpha/isalpha.c
+++ Libft/isalpha/isalpha.c
@@ -6,8 +6,24 @@
 /*   By: mteriier <mteriier@student42.lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 18:06:54 by mteriier          #+#    #+#             */
-/*   Updated: 2025/10/12 18:07:28 by mteriier         ###   ########.fr       */
+/*   Updated: 2025/10/12 18:33:00 by mteriier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1024);
+	}
+	return (0);
+}
+/*
 #include <stdio.h>
+#include <ctype.h>
+
+int	main(void)
+{
+	printf("program : %d\n", isalpha('f'));
+	printf("mine : %d\n", ft_isalpha('f'));
+}*/
