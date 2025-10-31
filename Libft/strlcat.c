b172e8cc1490b4de5c719a773033fef26e1dc4fc
Maoake <maoake.terii@gmail.com>
2025-10-31 21:18:23 +0000

    finish on the tolower.c

--- Libft/strlcat.c
+++ Libft/strlcat.c
@@ -6,18 +6,45 @@
 /*   By: mteriier </var/spool/mail/mteriier>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 11:41:49 by mteriier          #+#    #+#             */
-/*   Updated: 2025/10/27 11:44:23 by mteriier         ###   ########.fr       */
+/*   Updated: 2025/10/31 20:52:39 by mteriier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t	i;
+	size_t	j;
 
+	i = ft_strlen(dst);
+	j = 0;
+	while (j < size && src[j])
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(dst));
 }
 
+/*#include <stdio.h>
+#include <string.h>
 int	main(void)
 {
-
-}
+	char	str1[10] = "test";
+	char	str2[10] = "ffff";
+	printf("%ld\n", ft_strlcat(str1, str2,12));
+	printf("%s\n", str1);
+}*/
